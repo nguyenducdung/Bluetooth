8a248aa35bfcb01cb245d708bcb512d9e912b61a
nguyenducdung <nguyenducdung1803@gmail.com>
2018-11-27 18:36:34 +0700

    Initial commit

---
+++ app/src/main/java/bkhust/nguyenducdung/com/bluetooth/MainActivity.java
@@ -0,0 +1,13 @@
+package bkhust.nguyenducdung.com.bluetooth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
